123ddf6ba5d080b53280f2b0cc8273537a5b09e8
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2022-09-19 11:03:01 +0200

    Add the communication firmware to the package.

--- plugin/installer/lua5.1/install_linux.lua
+++ plugin/installer/lua5.1/install_linux.lua
@@ -5,6 +5,7 @@ local tResult
 if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strDistVersion=='@JONCHKI_PLATFORM_DIST_VERSION@' and strCpuArch=='@JONCHKI_PLATFORM_CPU_ARCH@' then
   t:install('lua_plugins/', '${install_lua_cpath}/')
   t:install('lua/', '${install_lua_path}/')
+  t:install('netx/', '${install_base}/netx/')
   tResult = true
 end
 
